2d28e8fbdbc8baaf83e5587675a39960e2c2e867
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2021-06-27 15:21:37 +0200

    spring data money transfer

--- src/main/java/pl/sda/service/MoneyTransferExecutor.java
+++ src/main/java/pl/sda/service/MoneyTransferExecutor.java
@@ -1,14 +1,23 @@
 package pl.sda.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.sda.model.AccountEntity;
+import pl.sda.repository.AccountRepository;
 
 import javax.transaction.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class MoneyTransferExecutor {
 
+    private final AccountRepository accountRepository;
+
     @Transactional
-    public void send(String senderAccount, String receiverAccount) {
+    public void send(String senderAccountNumber, String receiverAccountNumber) {
+        AccountEntity senderAccount = accountRepository.findFirstByAccountNumber(senderAccountNumber);
+        AccountEntity receiverAccount = accountRepository.findFirstByAccountNumber(receiverAccountNumber);
+
 
     }
 }
